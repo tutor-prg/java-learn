68eac79ab71b3e683dd777a4c9c5e987d22cdbb2
Hennadii <asm777@ukr.net>
2024-07-10 12:58:12 +0300

    function sub() has been added to project

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,18 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        int k = add(3, 7);
-        System.out.println("k = " + k);
+        int q = add(3, 7);
+        System.out.println("q = " + q);
+
+        int p = sub(3, 7);
+        System.out.println("p = " + p);
     }
 
     static int add(int a, int b){
         return a + b;
+    }
+
+    static int sub(int a, int b){
+        return a - b;
     }
 }

26534218f38206984fb4277fd52d24e5c538c206
Hennadii <asm777@ukr.net>
2024-07-10 12:50:04 +0300

    function int add(a, b)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        int k = add(3, 7);
+        System.out.println("k = " + k);
+    }
+
+    static int add(int a, int b){
+        return a + b;
     }
 }
